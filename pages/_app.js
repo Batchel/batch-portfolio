3254fe5ec31fc694a45a5abc66960c09608b8776
Satchel <sbbirch@ucsd.edu>
2024-02-11 14:15:39 -0800

    analytics deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import styles from '../styles/globals.css'
 import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <>
+  return (<>
   <Navbar/>
   <Component {...pageProps} />
+  <Analytics />
   <Footer/>
   </>
+  );
 }
 
 export default MyApp
